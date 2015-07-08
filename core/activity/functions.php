47a0b4b1c55a89c6f40fb175595d5acdcd302fc4
Brajesh Singh <brajesh@cosmiccoders.com>
2015-07-09 01:18:43 +0530

    Drop _mpp_record_activity and update mpp_record_activity for generating media/gallery activity

--- core/activity/functions.php
+++ core/activity/functions.php
@@ -266,97 +266,6 @@ function mpp_activity_mark_attached_media( $activity_id  ) {
 
 
 /**
- * Record MediaPress activity
- * We piggyback on others
- * 
- * 
- * @param type $media_ids
- * @todo remove in future as we don't need it 
- */
-function mpp_record_activity( $args = array() ) {
-	
-	$default = array(
-		'media_ids'		=> false,
-		'gallery_id'	=> 0,
-		'media_id'		=> 0,
-		'content'		=> '',
-		'component'		=> '',
-		'component_id'	=> 0,
-	);
-
-	$args = wp_parse_args( $args, $default );
-	
-	$media_ids = $args['media_ids'];
-	
-	if( empty( $media_ids ) ) {
-		$media_ids = array();
-	}	
-	
-	if( ! empty( $args['media_id'] ) ) {
-		
-		array_push ( $media_ids, $args['media_id'] );
-	}
-	
-	$args['media_ids'] = $media_ids;
-	
-	$activity_id = _mpp_record_activity( $args );
-	
-	//unable to save or anything
-	if( ! $activity_id )
-		return false;
-	
-	if( $args['gallery_id'] ) {
-		
-		$gallery = mpp_get_gallery( absint( $args['gallery_id'] ) );
-		
-		$component		= $gallery->component;
-		$component_id	= $gallery->component_id;
-		
-	} else {
-		
-		if( empty( $media_ids ) )
-			return;
-		
-		$media_id = $media_ids[0];
-		
-		$media = mpp_get_media( $media_id );
-		
-		$component = $media->component;
-		
-		$component_id = $media->component_id;
-		
-		$gallery = mpp_get_gallery( $media->gallery_id );
-		
-	}
-	
-	if( empty( $gallery ) )
-		return false;
-	
-	$activity = new BP_Activity_Activity( $activity_id );
-	
-	$hide_sitewide = $activity->hide_sitewide;
-	
-	$status = mpp_get_gallery_status( $gallery );
-	
-	//for non public galleries
-	if( $status != 'public' )
-		$hide_sitewide = 1;
-	
-	$activity->type = 'mpp_media_upload';
-	$activity->hide_sitewide = $hide_sitewide;
-	//save
-    $activity->save();
-	
-	if( ! empty( $media_ids ) ) {
-		mpp_activity_update_attached_media_ids( $activity_id, $media_ids );
-	}
-	mpp_activity_update_gallery_id( $activity_id, $gallery->id );
-	
-	return true;
-	
-}
-
-/**
  * Record Media Activity
  * 
  * It does not actually records activity, simply simulates the activity update and rest are done by the actions.php functions
@@ -365,75 +274,93 @@ function mpp_record_activity( $args = array() ) {
  * @param type $args
  * @return boolean
  */
-function _mpp_record_activity( $args = null ) {
+function mpp_record_activity( $args = null ) {
 	
 	//if activity module is not active, why bother
 	if( ! bp_is_active( 'activity' ) ) {
 		return false; 
 	}
-	
+
 	$default = array(
 		'gallery_id'	=> 0,
-		'media_ids'		=> '',
+		'media_id'		=> 0,
+		'media_ids'		=> null,//single id or an array of ids
+		'action'		=> '', 
+		'content'		=> '',
+		'type'			=> '',//type of activity  'create_gallery, update_gallery, media_upload etc'
 		'component'		=> mpp_get_current_component(),
 		'component_id'	=> mpp_get_current_component_id(),
 		'user_id'		=> get_current_user_id(),
-		'action'		=> '',
-		'content'		=> '',
+		'status'		=> 0,
+	
 	);
 	
 	$args = wp_parse_args( $args, $default );
-	
-	//if media ids are not provided or component is not given or user id is not given, we will not record the activity
-	if( empty( $args['media_ids'] ) || empty( $args['component'] ) || empty( $args['user_id'] ) || empty( $args['component_id'] ) ) {
-	
+	//atleast a gallery id or a media id should be given
+	if(  ( ! $args['gallery_id'] && ! $args['media_id'] ) || ! mpp_is_active_component( $args['component'] ) || ! $args['component_id'] ) {
 		return false;
-	}	
+	}
+	
+	$gallery_id = absint( $args['gallery_id'] );
+	$media_id = absint( $args['media_id'] );
+	
+	$hide_sitewide = 0;
+	
+	if( $args['status'] != 'public' ) {
+		$hide_sitewide = 1;
+	}
 	
 	$media_ids = $args['media_ids'];
 	
-	//sanitize ids
-	$media_ids = wp_parse_id_list( $media_ids );
+	if( ! empty( $media_ids ) && ! is_array( $media_ids ) ) {
+		$media_ids = explode( ',', $media_ids );
+	}
 	
-	$media_count = count( $media_ids );
-	//get the first media
-	$media = mpp_get_media( $media_ids[0] );
+	$component = $args['component'];
 	
-	$type = $media->type;
-	//we need the type plural in case of mult
-	$type = _n( $type, $type . 's', $media_count );//photo vs photos etc
+	if( $component == buddypress()->members->id ) {
+		
+		$component = buddypress()->activity->id; //for user gallery updates, let it be simple activity , do not set the component to 'members'
+	}
 	
-	if( empty( $args['content'] ) ) {
-		
-		$content = sprintf( __( 'Added %s', 'mediapress' ), $type );
-		
-	} else {
-		
-		$content = $args['content'];
-	}	
-	//here is the way to shortcircuit the things
-	//fake media ids in cookie
-	//$_COOKIE['_mpp_activity_attached_media_ids'] = join( ',', $media_ids );//it is expected as comma separated
+	$activity_id = bp_activity_add( array(
+		'id'                => false,
+		'user_id'           => $args['user_id'],
+		'action'            => $args['action'],
+		'content'           => $args['content'],
+		//'primary_link'      => '',
+		'component'         => $component,
+		'type'              => 'mpp_media_upload',
+		'item_id'           => absint( $args['component_id'] ),
+		'secondary_item_id' => false,
+		'recorded_time'     => bp_core_current_time(),
+		'hide_sitewide'     => $hide_sitewide
+	) );
 	
+	if( ! $activity_id ) {
+		return false;//there was a problem
+		
+	}
 	
-	if( $args['component'] == 'groups' && bp_is_active( 'groups') ) {
-		
-		
-		$activity_id = groups_post_update( array(
-			'group_id'	=> $args['component_id'],
-			'content'	=> $content,
-			'user_id'	=> $args['user_id']
-		) );
-		
-	} else {
+	//store the type of gallery activity in meta
 	
-		$activity_id = bp_activity_post_update( array(
-			'user_id'	=> $args['user_id'],
-			'content'	=> $content,
-		) );
-		
-		//simply post to activity stream
+	if( $type ) {
+		mpp_activity_update_activity_type( $activity_id, $type );
+	}
+	
+	if( $media_ids ) {
+		$media_ids = wp_parse_id_list( $media_ids );
+		mpp_activity_update_attached_media_ids( $activity_id, $media_ids );
+	}
+	
+	if( $gallery_id ) {
+		mpp_activity_update_gallery_id( $activity_id, $gallery_id );
+	}
+	
+	if( $media_id ) {
+		mpp_activity_update_media_id( $activity_id, $media_id );
 	}
 	
 	return $activity_id;
+	
 }
