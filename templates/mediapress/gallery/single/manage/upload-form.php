3da5f9a583268df42fe701d1e009a2793a23663b
Brajesh Singh <brajesh@cosmiccoders.com>
2015-08-18 05:47:42 +0530

    Add a few actions to allow hooking to the upload section of a gallery upload form

--- templates/mediapress/gallery/single/manage/upload-form.php
+++ templates/mediapress/gallery/single/manage/upload-form.php
@@ -6,20 +6,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <!-- append uploaded media here -->
-        <div id="mpp-gallery-media-list" class="mpp-uploading-media-list">
-            <ul> 
-           
-            </ul>
-        </div>
-		
-        <!-- drop files here for uploading -->
-        <div id="mpp-gallery-dropzone" class="mpp-dropzone">
-            <button id="mpp-add-gallery-media"><?php _e( 'Add media', 'mediapress' );?></button>
-        </div>
-        <!-- show any feedback here -->
-        <div id="mpp-gallery-upload-feedback" class="mpp-feedback">
-            <ul> </ul>
-        </div>
-		
-		<input type='hidden' name='mpp-context' id='mpp-context' value='gallery' />
-		<input type='hidden' name='mpp-upload-gallery-id' id='mpp-upload-gallery-id' value="<?php echo mpp_get_current_gallery_id() ;?>" />
+<div id="mpp-gallery-media-list" class="mpp-uploading-media-list">
+	<ul> 
+
+	</ul>
+</div>
+<?php do_action( 'mpp_after_gallery_upload_medialist' );?>		
+<!-- drop files here for uploading -->
+<div id="mpp-gallery-dropzone" class="mpp-dropzone">
+	<button id="mpp-add-gallery-media"><?php _e( 'Add media', 'mediapress' );?></button>
+</div>
+<?php do_action( 'mpp_after_gallery_upload_dropzone' );?>
+<!-- show any feedback here -->
+<div id="mpp-gallery-upload-feedback" class="mpp-feedback">
+	<ul> </ul>
+</div>
+<?php do_action( 'mpp_after_gallery_upload_feedback' );?>
+<input type='hidden' name='mpp-context' id='mpp-context' value='gallery' />
+<input type='hidden' name='mpp-upload-gallery-id' id='mpp-upload-gallery-id' value="<?php echo mpp_get_current_gallery_id() ;?>" />
