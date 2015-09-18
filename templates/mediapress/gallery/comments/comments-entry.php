d3bf9d157ba55283f22811faebfa3fc7926a703a
Brajesh Singh <brajesh@cosmiccoders.com>
2015-09-18 10:09:29 +0530

    Mistaken commit. A hell lot of changes got in.

--- templates/mediapress/gallery/comments/comments-entry.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Exit if the file is accessed directly over web
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
-}
-/**
- * Not Used in version 1.0.0
- */
--- templates/mediapress/gallery/comments/comments-loop.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Exit if the file is accessed directly over web
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
-}
-/**
- * Not Used in version 1.0.0
- */
-
