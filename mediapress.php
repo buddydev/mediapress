a58b3983f026f42af72c5d1ed2e341132d456c12
Brajesh Singh <brajesh@cosmiccoders.com>
2015-09-26 16:54:27 +0530

    Delegate the file loading responsibility to loader

--- mediapress.php
+++ mediapress.php
@@ -26,8 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) {
  * 
  */
 
-
-
 class MediaPress {
 	/**
 	 *
@@ -90,13 +88,13 @@ class MediaPress {
 	 * 
 	 * @var MPP_Gallery_Query
 	 */
-	public $the_gallery_query; //main gallery query
+	public $the_gallery_query = null; //main gallery query
 
 	/**
 	 *
 	 * @var MPP_Media_Query 
 	 */
-	public $the_media_query; //main media query
+	public $the_media_query = null; //main media query
 
 	/**
 	 * Current Gallery object is stored here
@@ -298,8 +296,8 @@ class MediaPress {
 		global $wpdb;
 		//logs table name
 		$this->store_table_name( 'logs', $wpdb->prefix . 'mpp_logs' );
-		
-		register_activation_hook( __FILE__, array( $this, 'do_activation' ) );
+		//register_activation_hook
+		add_action( 'activate_' . $this->basename, array( $this, 'do_activation' ) );
 		
 		add_action( 'plugins_loaded', array( $this, 'load' ), 0 );
 				
@@ -310,164 +308,12 @@ class MediaPress {
 	
 	public function load() {
 		
-		$this->load_core();
-		$this->load_ajax_handlers();
+		require_once $this->plugin_path . 'mpp-loader.php';
+		
+		$loader = new MPP_Core_Loader();
+		$loader->load();
 		
 		do_action( 'mpp_loaded' );
-	}
-
-	public function load_core() {
-
-		$files	 = array(
-			
-			'core/common/mpp-feedback-functions.php',
-			'core/common/mpp-misc-functions.php',
-			'core/common/mpp-common-functions.php',
-			'core/common/init.php',
-			'core/common/class-mpp-cached-media-query.php',
-			'core/common/class-mpp-gallery-query.php',
-			'core/common/class-mpp-media-query.php',
-			'core/common/mpp-nav-functions.php',
-			'core/mpp-post-type.php',
-			'core/class-mpp-deletion-actions-mapper.php',
-			'core/common/class-mpp-taxonomy.php',
-			'core/common/class-mpp-menu.php',
-			'core/common/class-mpp-features.php',
-			'core/common/mpp-taxonomy-functions.php',
-			
-			
-			'core/gallery/class-mpp-gallery.php',
-			'core/gallery/mpp-gallery-conditionals.php',
-			'core/gallery/mpp-gallery-cover-templates.php',
-			'core/gallery/mpp-gallery-functions.php',
-			'core/gallery/mpp-gallery-link-template.php',
-			'core/gallery/mpp-gallery-meta.php',
-			'core/gallery/mpp-gallery-screen.php',
-			'core/gallery/mpp-gallery-template-tags.php',
-			'core/gallery/mpp-gallery-hooks.php',
-			'core/gallery/mpp-gallery-actions.php',
-			'core/gallery/mpp-gallery-activity.php',
-			'core/gallery/mpp-gallery-template.php',
-			
-			'core/media/mpp-media-functions.php',
-			'core/media/mpp-media-meta.php',
-			'core/media/class-mpp-media.php',
-			'core/media/mpp-media-template-tags.php',
-			'core/media/mpp-media-link-templates.php',
-			'core/media/mpp-media-actions.php',
-			'core/media/mpp-media-cover-template.php',
-			'core/media/mpp-media-activity.php',
-			'core/media/mpp-media-hooks.php',
-			
-			//gallery views
-			'core/views/class-mpp-gallery-view.php',
-			'core/views/class-mpp-gallery-view-default.php',
-			'core/views/class-mpp-gallery-view-audio-playlist.php',
-			'core/views/class-mpp-gallery-view-video-playlist.php',
-			'core/views/class-mpp-gallery-view-list.php',
-			'core/views/mpp-gallery-view-functions.php',
-			
-			//media viewer
-			'core/media/views/class-mpp-media-view.php',
-			'core/media/views/class-mpp-media-view-photo.php', //for doc files
-			'core/media/views/class-mpp-media-view-doc.php', //for doc files
-			'core/media/views/class-mpp-media-view-video.php', //for video files
-			'core/media/views/class-mpp-media-view-audio.php', //for audio files
-			
-			//api
-			'core/api/mpp-actions-api.php',
-			'core/api/mpp-api.php',
-			'core/mpp-hooks.php',
-			//user
-			'core/users/mpp-user-meta.php',
-			'core/users/mpp-user-functions.php',
-			'core/users/mpp-user-hooks.php',
-			//activity, we will move all activity related functions to BuddyPress module in future, that will cut down loading
-
-
-			//component loader
-			
-			
-			'assets/mpp-script-loader.php',
-
-			
-			'core/mpp-template-helpers.php',
-			'core/mpp-permissions.php',
-			
-			//storage related
-			'core/storage/mpp-storage-functions.php',
-			'core/storage/mpp-storage-space-stats-functions.php',
-			'core/storage/class-mpp-storage-manager.php',
-			'core/storage/class-mpp-local-storage.php',
-			
-			'core/shortcodes/mpp-shortcode-functions.php',
-			'core/shortcodes/mpp-shortcode-gallery-list.php',
-			'core/shortcodes/mpp-shortcode-media-list.php',
-			'core/shortcodes/mpp-shortcode-gallery-create.php',
-			
-			'core/widgets/mpp-widget-functions.php',
-			'core/widgets/mpp-widget-gallery.php',
-			'core/widgets/mpp-widget-media.php',
-			
-			
-			//theme compat
-			'core/mpp-theme-compat.php',
-			
-
-			'mpp-core-component.php',
-			
-		);
-		
-		if( is_admin() ) {
-			$files[] = 'admin/mpp-admin-loader.php';
-		}
-		
-		if( $this->is_bp_active() ) {
-			$files[] = 'modules/buddypress/mpp-bp-loader.php';
-		}
-		
-		$path		= $this->get_path();
-		
-		foreach ( $files as $file ) {
-			require_once $path . $file;
-		}
-	}
-
-	/**
-	 * Load ajax handlers
-	 * 
-	 * @return type
-	 */
-	private function load_ajax_handlers() {
-		
-		if( !  defined( 'DOING_AJAX' ) ) {
-			return ;
-		}
-		
-		$files = array(
-			'core/mpp-ajax.php',
-			'core/ajax/class-mpp-ajax-comment-helper.php',
-		);
-		
-		$path		= $this->get_path();
-		
-		foreach( $files as $file ) {
-			require_once $path . $file;
-		}
-		
-	}
-	
-	/**
-	 * Load comments handlers
-	 */
-	private function load_comment_handlers() {
-		
-		$path = $this->path;
-					//comment
-		require_once  $path . 'core/comments/mpp-comment-functions.php';
-		require_once  $path . 'core/comments/class-mpp-comment.php';
-		require_once  $path . 'core/comments/class-mpp-comments-helper.php';
-		require_once  $path . 'core/comments/mpp-comment-template-tags.php';
 	}
 
 	/**
@@ -488,16 +334,15 @@ class MediaPress {
 		require_once $this->plugin_path . 'admin/mpp-admin-install.php';
 		mpp_install_db();
 	}
-	
-
 	/**
 	 * Load textdomain
 	 * 
 	 */
     public function load_textdomain() {
         
-		load_plugin_textdomain( 'mediapress', FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages'  );
+		load_plugin_textdomain( 'mediapress', FALSE, dirname( $this->basename ) . '/languages'  );
 	}
+
 	/**
 	 * Get the url of the MediaPress plugin directory ( e.g http://site.com/wp-content/plugins/mediapress/)
 	 * 
