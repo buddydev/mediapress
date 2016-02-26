ed299494df7215651bd7c30750f2a537e7657e4d
Brajesh Singh <brajesh@cosmiccoders.com>
2016-02-27 04:02:30 +0530

    Add Gallery pagination support in shortcode, directly include template file to get access to all variables and a little more

--- core/shortcodes/mpp-shortcode-gallery-list.php
+++ core/shortcodes/mpp-shortcode-gallery-list.php
@@ -5,7 +5,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Galler Listing shortcode
+ * Gallery Listing shortcode
  */
 add_shortcode( 'mpp-list-gallery', 'mpp_shortcode_list_gallery' );
 
@@ -23,7 +23,7 @@ function mpp_shortcode_list_gallery( $atts = null, $content = '' ) {
                 'component_id'  => false,// the associated component id, could be group id, user id, event id
                 'per_page'      => false, //how many items per page
                 'offset'        => false, //how many galleries to offset/displace
-                'page'          => false,//which page when paged
+                'page'          => isset( $_REQUEST['gpage'] ) ? absint( $_REQUEST['gpage'] ) : false,//which page when paged
                 'nopaging'      => false, //to avoid paging
                 'order'         => 'DESC',//order 
                 'orderby'       => 'date',//none, id, user, title, slug, date,modified, random, comment_count, meta_value,meta_value_num, ids
@@ -46,8 +46,9 @@ function mpp_shortcode_list_gallery( $atts = null, $content = '' ) {
                 'meta_key'		=> '',
                 'meta_value'	=> '',
                // 'meta_query'=>false,
-                'fields'    => false,//which fields to return ids, id=>parent, all fields(default)
-				'column'	=> 4,
+                'fields'        => false,//which fields to return ids, id=>parent, all fields(default)
+				'column'	    => 4,
+	            'show_pagination'   => 1, //show the pagination links?
         );
         
     $atts = shortcode_atts( $defaults, $atts );
@@ -56,141 +57,139 @@ function mpp_shortcode_list_gallery( $atts = null, $content = '' ) {
         unset( $atts['meta_key'] );
         unset( $atts['meta_value'] );
     }
-    
+    //These variables are used in the template
 	$shortcode_column = $atts['column'];
-	mpp_shortcode_save_gallery_data( 'column', $shortcode_column );
-	
-	
+	$show_pagination = $atts['show_pagination'];
+
 	unset( $atts['column'] );
 	//unset( $atts['view'] );
+
+	//the query is available in the shortcode template
 	$query = new MPP_Gallery_Query( $atts );
-	
-	mpp_shortcode_save_gallery_data( 'gallery_list_query', $query );
-    
-    ob_start();
-    
-	//include temlate
-	
-	mpp_get_template( 'shortcodes/gallery-list.php' );
+
+	$located = mpp_locate_template( array( 'shortcodes/gallery-list.php' ), false );
+
+	ob_start();
+    //include shortcode template
+	if ( $located ) {
+		require $located;
+	}
 	
     $content = ob_get_clean();
-	
-	mpp_shortcode_reset_gallery_data( 'column' );
-    mpp_shortcode_reset_gallery_data( 'gallery_list_query' );
-	
+
     return $content;
 }
 
 add_shortcode( 'mpp-show-gallery', 'mpp_shortcode_show_gallery' );
 
 function mpp_shortcode_show_gallery( $atts = null, $content = '' ) {
-    
-        $defaults = array(
-			'id'            => false, //pass specific gallery id
-			'in'            => false, //pass specific gallery ids as array
-			'exclude'       => false, //pass gallery ids to exclude
-			'slug'          => false,//pass gallery slug to include
-			'per_page'      => false, //how many items per page
-			'offset'        => false, //how many galleries to offset/displace
-			'page'          => false,//which page when paged
-			'nopaging'      => false, //to avoid paging
-			'order'         => 'DESC',//order 
-			'orderby'       => 'date',//none, id, user, title, slug, date,modified, random, comment_count, meta_value,meta_value_num, ids
-			//user params
-			'user_id'       => false,
-			'include_users' => false,
-			'exclude_users' => false,//users to exclude
-			'user_name'     => false,
-			'scope'         => false,
-			'search_terms'  => '',
 
-			'meta_key'		=> '',
-			'meta_value'	=> '',
-			'column'		=> 4,
-			'view'			=> '',
-        );
-        
+	$defaults = array(
+		'id'            => false, //pass specific gallery id
+		'in'            => false, //pass specific gallery ids as array
+		'exclude'       => false, //pass gallery ids to exclude
+		'slug'          => false,//pass gallery slug to include
+		'per_page'      => false, //how many items per page
+		'offset'        => false, //how many galleries to offset/displace
+		'page'          => false,//which page when paged
+		'nopaging'      => false, //to avoid paging
+		'order'         => 'DESC',//order
+		'orderby'       => 'date',//none, id, user, title, slug, date,modified, random, comment_count, meta_value,meta_value_num, ids
+		//user params
+		'user_id'       => false,
+		'include_users' => false,
+		'exclude_users' => false,//users to exclude
+		'user_name'     => false,
+		'scope'         => false,
+		'search_terms'  => '',
+
+		'meta_key'		=> '',
+		'meta_value'	=> '',
+		'column'		=> 4,
+		'view'			=> '',
+	);
+
 	$defaults = apply_filters( 'mpp_shortcode_show_gallery_defaults', $defaults );
-	
-    $atts = shortcode_atts( $defaults, $atts );
-    
+
+	$atts = shortcode_atts( $defaults, $atts );
+
 	if ( ! $atts['id'] ) {
 		return '';
 	}
-	
+
 	$gallery_id = absint( $atts['id'] );
-	
+
 	global $wpdb;
-	
-	$attachments = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM {$wpdb->posts} WHERE post_parent = %d AND post_type = %s ", $gallery_id, 'attachment' ) ); 
-	
+
+	$attachments = $wpdb->get_col( $wpdb->prepare( "SELECT ID FROM {$wpdb->posts} WHERE post_parent = %d AND post_type = %s ", $gallery_id, 'attachment' ) );
+
 	array_push( $attachments, $gallery_id );
-	
+
 	_prime_post_caches( $attachments, true, true );
-	
+
 	$gallery = mpp_get_gallery( $gallery_id );
 	//if gallery does not exist, there is no proint in further proceeding
 	if ( ! $gallery ) {
 		return '';
 	}
-	
-	
-    if ( ! $atts['meta_key'] ) {
-        
-        unset( $atts['meta_key'] );
-        unset( $atts['meta_value'] );
-    }
-    
+
+
+	if ( ! $atts['meta_key'] ) {
+
+		unset( $atts['meta_key'] );
+		unset( $atts['meta_value'] );
+	}
+
 	$view = $atts['view'];
-	
+
 	unset( $atts['id'] );
 	unset( $atts['view'] );
-	
+
 	$atts['gallery_id'] = $gallery_id;
 
 	$shortcode_column = $atts['column'];
 	mpp_shortcode_save_media_data( 'column', $shortcode_column );
-	
+
 	mpp_shortcode_save_media_data( 'shortcode_args', $atts );
-	
+
 	unset( $atts['column'] );
-	
+
 	$atts = array_filter( $atts );
-	
+
 	$query = new MPP_Media_Query( $atts );
 	mpp_shortcode_save_media_data( 'query', $query );
-		
+
 	$content = apply_filters( 'mpp_shortcode_mpp_show_gallery_content', '', $atts, $view );
-	
+
 	if( ! $content ) {
-		
+
 		$templates = array(
 			'shortcodes/grid.php'
-	
+
 		);
-		
+
 		if ( $view ) {
 			$type = $gallery->type;
-					
+
 			$preferred_templates = array(
 				"shortcodes/{$view}-{$type}.php",
 				"shortcodes/{$view}.php",
 			);//audio-playlist, video-playlist
-			
+
 			$templates = array_merge( $preferred_templates, $templates );
 			//array_unshift( $templates, $preferred_template );
 		}
-		
+
 		ob_start();
-		
+
 		mpp_locate_template( $templates,  true );//load
-		 
+
 		$content = ob_get_clean();
 	}
-	
+
 	mpp_shortcode_reset_media_data( 'column' );
 	mpp_shortcode_reset_media_data( 'query' );
 	mpp_shortcode_reset_media_data( 'shortcode_args' );
-    
-    return $content;
+
+	return $content;
 }
