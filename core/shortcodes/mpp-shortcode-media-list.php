e0d6dbe1e1b2e2c233cc4380d05abefd9283836d
Brajesh Singh <brajesh@cosmiccoders.com>
2016-03-11 21:10:14 +0530

    reformat list media rags array

--- core/shortcodes/mpp-shortcode-media-list.php
+++ core/shortcodes/mpp-shortcode-media-list.php
@@ -16,52 +16,76 @@ add_shortcode( 'mpp-media', 'mpp_shortcode_media_list' );
 
 function mpp_shortcode_media_list( $atts = null, $content = '' ) {
     //allow everything that can be done to be passed via this shortcode
-    $default_status = mpp_is_active_status('public') ? 'public' : mpp_get_default_status();
-        $defaults = array(
-				'view'				=> 'grid',
-                'type'				=> false, //gallery type, all,audio,video,photo etc
-                'id'				=> false, //pass specific media id
-                'in'				=> false, //pass specific media ids as array
-                'exclude'			=> false, //pass gallery ids to exclude
-                'slug'				=> false, //pass gallery slug to include
-                'status'			=> $default_status, //public,private,friends one or more privacy level
-                'component'			=> false, //one or more component name user,groups, evenets etc
-                'component_id'		=> false, // the associated component id, could be group id, user id, event id
-                'gallery_id'		=> false,
-                'galleries'			=> false,
-                'galleries_exclude'	=> false,
-            
-                'per_page'			=> false, //how many items per page
-                'offset'			=> false, //how many galleries to offset/displace
-				'page'          => isset( $_REQUEST['mpage'] ) ? absint( $_REQUEST['mpage'] ) : false,//which page when paged
-                'nopaging'			=> false, //to avoid paging
-                'order'				=> 'DESC',//order 
-                'orderby'			=> 'date',//none, id, user, title, slug, date,modified, random, comment_count, meta_value,meta_value_num, ids
-                //user params
-                'user_id'			=> false,
-                'include_users'		=> false,
-                'exclude_users'		=> false,//users to exclude
-                'user_name'			=> false,
-                'scope'				=> false,
-                'search_terms'		=> '',
-            //time parameter
-                'year'				=> false,//this years
-                'month'				=> false,//1-12 month number
-                'week'				=> '', //1-53 week
-                'day'				=> '',//specific day
-                'hour'				=> '',//specific hour
-                'minute'			=> '', //specific minute
-                'second'			=> '',//specific second 0-60
-                'yearmonth'			=> false,// yearMonth, 201307//july 2013
-                'meta_key'			=> '',
-                'meta_value'		=> '',
-				'column'			=> 4,
-				'playlist'			=> 0,
-               // 'meta_query'=>false,
-                'fields'			=> false,//which fields to return ids, id=>parent, all fields(default)
-	            'show_pagination'   => 1,
-	            'lightbox'          => 0
-        );
+	$default_status = mpp_is_active_status( 'public' ) ? 'public' : mpp_get_default_status();
+	$defaults       = array(
+		'view'              => 'grid',
+		'type'              => false,
+		//gallery type, all,audio,video,photo etc
+		'id'                => false,
+		//pass specific media id
+		'in'                => false,
+		//pass specific media ids as array
+		'exclude'           => false,
+		//pass gallery ids to exclude
+		'slug'              => false,
+		//pass gallery slug to include
+		'status'            => $default_status,
+		//public,private,friends one or more privacy level
+		'component'         => false,
+		//one or more component name user,groups, evenets etc
+		'component_id'      => false,
+		// the associated component id, could be group id, user id, event id
+		'gallery_id'        => false,
+		'galleries'         => false,
+		'galleries_exclude' => false,
+
+		'per_page'        => false,
+		//how many items per page
+		'offset'          => false,
+		//how many galleries to offset/displace
+		'page'            => isset( $_REQUEST['mpage'] ) ? absint( $_REQUEST['mpage'] ) : false,
+		//which page when paged
+		'nopaging'        => false,
+		//to avoid paging
+		'order'           => 'DESC',
+		//order
+		'orderby'         => 'date',
+		//none, id, user, title, slug, date,modified, random, comment_count, meta_value,meta_value_num, ids
+		//user params
+		'user_id'         => false,
+		'include_users'   => false,
+		'exclude_users'   => false,
+		//users to exclude
+		'user_name'       => false,
+		'scope'           => false,
+		'search_terms'    => '',
+		//time parameter
+		'year'            => false,
+		//this years
+		'month'           => false,
+		//1-12 month number
+		'week'            => '',
+		//1-53 week
+		'day'             => '',
+		//specific day
+		'hour'            => '',
+		//specific hour
+		'minute'          => '',
+		//specific minute
+		'second'          => '',
+		//specific second 0-60
+		'yearmonth'       => false,
+		// yearMonth, 201307//july 2013
+		'meta_key'        => '',
+		'meta_value'      => '',
+		'column'          => 4,
+		'playlist'        => 0,
+		// 'meta_query'=>false,
+		'fields'          => false,
+		//which fields to return ids, id=>parent, all fields(default)
+		'show_pagination' => 1,
+		'lightbox'        => 0
+	);
 
 	$defaults = apply_filters( 'mpp_shortcode_list_media_defaults', $defaults );
     $atts = shortcode_atts( $defaults, $atts );
