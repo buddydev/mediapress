69c575ee901744deb670f8e8d5def29495ac012b
Brajesh Singh <brajesh@cosmiccoders.com>
2016-09-12 11:55:58 +0530

    Update source formatting for groups module

--- modules/buddypress/groups/mpp-bp-groups-actions.php
+++ modules/buddypress/groups/mpp-bp-groups-actions.php
@@ -1,21 +1,24 @@
 <?php
-
 // Exit if the file is accessed directly over web
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_action( 'groups_delete_group', 'mpp_delete_galleries_for_group' ); //group id
-
+/**
+ * Delete Galleries of the group when a group is deleted
+ *
+ * @param int $group_id
+ */
 function mpp_delete_galleries_for_group( $group_id ) {
 
 	//DELETE ALL Galleries
-
 	$query = new MPP_Gallery_Query( array( 'component_id' => $group_id, 'fields' => 'ids', 'component' => 'groups' ) );
-	$ids = $query->get_ids();
+	$ids   = $query->get_ids();
 
 	//Delete all galleries
 	foreach ( $ids as $gallery_id ) {
 		mpp_delete_gallery( $gallery_id );
 	}
 }
+
+add_action( 'groups_delete_group', 'mpp_delete_galleries_for_group' ); //group id
--- modules/buddypress/groups/mpp-bp-groups-functions.php
+++ modules/buddypress/groups/mpp-bp-groups-functions.php
@@ -4,8 +4,14 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-function mpp_group_is_gallery_enabled( $group_id = false ) {
+/**
+ * Checks if MediaPress is enabled for this group
+ *
+ * @param int $group_id
+ *
+ * @return bool
+ */
+function mpp_group_is_gallery_enabled( $group_id = 0 ) {
 
 	//is groups component enabled?
 	if ( mpp_is_enabled( 'groups', $group_id ) ) {
@@ -19,7 +25,6 @@ function mpp_group_is_gallery_enabled( $group_id = false ) {
 	}
 
 	if ( ! $group_id ) {
-
 		$group = groups_get_current_group();
 
 		if ( ! empty( $group ) ) {
@@ -30,7 +35,6 @@ function mpp_group_is_gallery_enabled( $group_id = false ) {
 	if ( ! $group_id ) {
 		return false;
 	}
-
 
 	//check for group settings
 	$is_enabled = groups_get_groupmeta( $group_id, '_mpp_is_enabled', true );
@@ -44,10 +48,11 @@ function mpp_group_is_gallery_enabled( $group_id = false ) {
 }
 
 /**
- * Set Gallery as enabled/disabled
- * 
- * @param type $group_id
- * @param type $enabled
+ * Sets MediaPress features as enabled/disabled for the given gallery
+ *
+ * @param int $group_id
+ * @param string $enabled yes|no
+ *
  * @return boolean
  */
 function mpp_group_set_gallery_state( $group_id = false, $enabled = 'yes' ) {
@@ -55,13 +60,12 @@ function mpp_group_set_gallery_state( $group_id = false, $enabled = 'yes' ) {
 	if ( ! $group_id ) {
 		$group_id = bp_get_group_id( groups_get_current_group() );
 	}
-	
+
 	if ( ! $group_id ) {
 		return false;
 	}
-	
-	//default settings from gloabl
 
+	//default settings from gloabl
 	$is_enabled = groups_update_groupmeta( $group_id, '_mpp_is_enabled', $enabled );
 
 	return $is_enabled;
@@ -70,110 +74,112 @@ function mpp_group_set_gallery_state( $group_id = false, $enabled = 'yes' ) {
 //for group wall galleries
 
 /**
- * Get wall photo gallery id
- * 
+ * Returns wall photo gallery id
+ *
  * @see mpp_get_wall_gallery_id()
- * 
- * @param type $group_id
- * @return type
+ *
+ * @param int $group_id
+ *
+ * @return int
  */
 function mpp_get_groups_wall_photo_gallery_id( $group_id ) {
-
 	return (int) groups_get_groupmeta( $group_id, '_mpp_wall_photo_gallery_id', true );
 }
 
 /**
- * Get wall Video gallery id
- * 
+ * Returns wall Video gallery id
+ *
  * @param type $group_id
+ *
  * @return type
  */
 function mpp_get_groups_wall_video_gallery_id( $group_id ) {
-
 	return (int) groups_get_groupmeta( $group_id, '_mpp_wall_video_gallery_id', true );
 }
 
 /**
  * Get wall audio gallery id
- * 
+ *
  * @see mpp_get_wall_gallery_id()
- * 
+ *
  * @param type $group_id
+ *
  * @return type
  */
 function mpp_get_groups_wall_audio_gallery_id( $group_id ) {
-
 	return (int) groups_get_groupmeta( $group_id, '_mpp_wall_audio_gallery_id', true );
 }
 
 /**
- * Get the document wall gallery id fro group
+ * Returns the document wall gallery id for group
  *
  * @param $group_id
  *
  * @return int
  */
 function mpp_get_groups_wall_doc_gallery_id( $group_id ) {
-
 	return (int) groups_get_groupmeta( $group_id, '_mpp_wall_doc_gallery_id', true );
 }
 
 /**
- * update wall photo gallery id
- * 
- * @see mpp_update_wall_gallery_id()
- * @param type $group_id
- * @return type
- */
-function mpp_update_groups_wall_photo_gallery_id( $group_id, $gallery_id ) {
-
-	return groups_update_groupmeta( $group_id, '_mpp_wall_photo_gallery_id', $gallery_id );
-}
-
-/**
- * update wall Video gallery id
- * 
- * @see mpp_update_wall_gallery_id()
- * 
- * @param type $group_id
- * @return type
- */
-function mpp_update_groups_wall_video_gallery_id( $group_id, $gallery_id ) {
-
-	return groups_update_groupmeta( $group_id, '_mpp_wall_video_gallery_id', $gallery_id );
-}
-
-/**
- * update wall audio gallery id
- * 
- * @see mpp_update_wall_gallery_id()
- * 
- * @param type $user_id
- * @return type
- */
-function mpp_update_groups_wall_audio_gallery_id( $group_id, $gallery_id ) {
-
-	return groups_update_groupmeta( $group_id, '_mpp_wall_audio_gallery_id', $gallery_id );
-}
-
-/**
- * update wall doc gallery id
+ * Updates wall photo gallery id
  *
  * @see mpp_update_wall_gallery_id()
  *
  * @param int $group_id
+ *
+ * @return int
+ */
+function mpp_update_groups_wall_photo_gallery_id( $group_id, $gallery_id ) {
+	return groups_update_groupmeta( $group_id, '_mpp_wall_photo_gallery_id', $gallery_id );
+}
+
+/**
+ * Updates wall Video gallery id
+ *
+ * @see mpp_update_wall_gallery_id()
+ *
+ * @param int $group_id
+ *
+ * @return int
+ */
+function mpp_update_groups_wall_video_gallery_id( $group_id, $gallery_id ) {
+	return groups_update_groupmeta( $group_id, '_mpp_wall_video_gallery_id', $gallery_id );
+}
+
+/**
+ * Updates wall audio gallery id
+ *
+ * @see mpp_update_wall_gallery_id()
+ *
+ * @param int $group_id
+ * @param int $gallery_id
+ *
+ * @return type
+ */
+function mpp_update_groups_wall_audio_gallery_id( $group_id, $gallery_id ) {
+	return groups_update_groupmeta( $group_id, '_mpp_wall_audio_gallery_id', $gallery_id );
+}
+
+/**
+ * Updates wall document(doc) gallery id
+ *
+ * @see mpp_update_wall_gallery_id()
+ *
+ * @param int $group_id
+ * @param int $gallery_id
+ *
  * @return int
  */
 function mpp_update_groups_wall_doc_gallery_id( $group_id, $gallery_id ) {
-
 	return groups_update_groupmeta( $group_id, '_mpp_wall_doc_gallery_id', $gallery_id );
 }
+
 /* * *
- * Delete
+ * Deletes Group wall gallery
  */
 
 function mpp_delete_groups_wall_gallery_id( $group_id, $type, $gallery_id ) {
-
 	$key = "_mpp_wall_{$type}_gallery_id";
 
 	return groups_delete_groupmeta( $group_id, $key, $gallery_id );
@@ -181,46 +187,47 @@ function mpp_delete_groups_wall_gallery_id( $group_id, $type, $gallery_id ) {
 
 /**
  * Checks if current user can access group's gallery/media or not
- * 
+ *
  * @see mpp_group_init() for use
- * 
+ *
  * @param string $component_type 'groups'
  * @param int $component_id current group id
  * @param int $user_id the user for which we are checking the access
- * 
+ *
  * @return boolean true if allowed, false if not allowed access
- * 
+ *
  */
 function mpp_check_groups_access( $component_type, $component_id, $user_id = null ) {
 
 	if ( ! $user_id ) {
 		$user_id = get_current_user_id();
 	}
-	
+
 	$allow = false;
 
 	if ( is_super_admin() || bp_is_active( 'groups' ) && ( groups_is_user_member( $user_id, $component_id ) ) ) {
 		$allow = true;
 	}
-	
+
 	return apply_filters( 'mpp_check_groups_access', $allow, $component_type, $component_id, $user_id );
 }
+
 /**
- * Is my Galleries filter enabled for the groups component
- * 
+ * Checks if 'My Galleries' filter is enabled for the groups component
+ *
  * @return boolean
  */
 function mpp_group_is_my_galleries_enabled() {
-	
 	return mpp_get_option( 'groups_enable_my_galleries' );
 }
-//a liitle bit deviation in maning here
+
+//a little bit deviation in naming here
 //but we will be moving more groups constructs tot his naming convention
 
 function mpp_group_get_user_galleries_url() {
-	
-	$component = 'groups';
+
+	$component    = 'groups';
 	$component_id = groups_get_current_group()->id;
-	
-	return user_trailingslashit( trailingslashit( mpp_get_gallery_base_url( $component, $component_id ) ) . 'my-gallery' ); 
+
+	return user_trailingslashit( trailingslashit( mpp_get_gallery_base_url( $component, $component_id ) ) . 'my-gallery' );
 }
