fa0d6849ee2e3e38a3cb505f37f4bd6fc5b870f5
Brajesh Singh <brajesh@cosmiccoders.com>
2017-04-24 18:38:46 +0530

    Use verb not adjective in the delete warning

--- templates/mediapress/default/gallery/manage/delete.php
+++ templates/mediapress/default/gallery/manage/delete.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <form method="post" action="" id="mpp-gallery-edit-form" class="mpp-form mpp-form-stacked mpp-gallery-delete-form">
 <div class="mpp-notice mpp-warning">
 
-	<p class="mpp-gallery-delete-warning"> <?php _e( 'Are you sure you want to delete this gallery? You will loose all the media!', 'mediapress' );?></p>
+	<p class="mpp-gallery-delete-warning"> <?php _e( 'Are you sure you want to delete this gallery? You will lose all the media!', 'mediapress' );?></p>
 
 	<?php do_action( 'mpp_gallery_delete_form_fields' ); ?>
 
