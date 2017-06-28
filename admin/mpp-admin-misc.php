e9858f5b2cf4bec1f7b9a73abb7b19a7bf2660fd
Brajesh Singh <brajesh@cosmiccoders.com>
2017-06-29 03:20:36 +0530

    Admin misc cleanup

--- admin/mpp-admin-misc.php
+++ admin/mpp-admin-misc.php
@@ -1,9 +1,23 @@
 <?php
 /**
- * Various sections for admin enhancement that does not fit at anoy other place will go here
+ * Various sections for admin enhancement that does not fit at any other place will go here.
+ *
+ * @package mediapress
  */
 
-//add "Galleries" link in the Admin User list
+// Exit if the file is accessed directly over web.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Add "View User Galleries" link in the Admin User list
+ *
+ * @param array   $actions action links.
+ * @param WP_User $user current row user.
+ *
+ * @return array
+ */
 function mpp_admin_user_row_actions( $actions = array(), $user = null ) {
 
 	if ( empty( $user->ID ) ) {
@@ -13,11 +27,11 @@ function mpp_admin_user_row_actions( $actions = array(), $user = null ) {
 	$url = mpp_admin()->get_menu_slug();
 
 	$url = add_query_arg( array(
-		'author' => $user->ID
+		'author' => $user->ID,
 	), $url );
 
-	$actions['view-mpp-gallery'] = sprintf( '<a href="%s" title="%s">%s</a>', $url , __( 'View User Galleries', 'mediapress'), __( 'Galleries', 'mediapress') );
+	$actions['view-mpp-gallery'] = sprintf( '<a href="%s" title="%s">%s</a>', $url, _x( 'View User Galleries', 'admin user list action link', 'mediapress' ), __( 'Galleries', 'mediapress' ) );
+
 	return $actions;
 }
-
 add_filter( 'user_row_actions', 'mpp_admin_user_row_actions', 10, 2 );
