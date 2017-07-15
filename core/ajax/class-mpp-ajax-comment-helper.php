7a9e49e2c3e2ad79ebac4bcc0c4e70443a716c8b
Brajesh Singh <brajesh@cosmiccoders.com>
2017-07-16 05:24:02 +0530

    Cleaing up docs/formatting for ajax handlers

--- core/ajax/class-mpp-ajax-comment-helper.php
+++ core/ajax/class-mpp-ajax-comment-helper.php
@@ -1,23 +1,30 @@
 <?php
-//No direct access to the file 
+// No direct access to the file.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit( 0 );
 }
+
 /**
- * MediaPress Ajax Commet Helper, handles posting of activiy comment/replies on the Gallery/media
- * 
+ * MediaPress Ajax Comment Helper, handles posting of activity comment/replies on the Gallery/media
  */
 class MPP_Ajax_Comment_Helper {
-
+	/**
+	 * Singleton instance
+	 *
+	 * @var MPP_Ajax_Comment_Helper
+	 */
 	private static $instance;
 
+	/**
+	 * Constructor
+	 */
 	private function __construct() {
-
 		$this->setup_hooks();
 	}
 
 	/**
-	 * 
+	 * Get the sngleton instance.
+	 *
 	 * @return MPP_Ajax_Comment_Helper
 	 */
 	public static function get_instance() {
@@ -25,65 +32,66 @@ class MPP_Ajax_Comment_Helper {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new self();
 		}
-		
+
 		return self::$instance;
 	}
 
+	/**
+	 * Setup hooks.
+	 */
 	private function setup_hooks() {
-		
 		add_action( 'wp_ajax_mpp_add_comment', array( $this, 'post_comment' ) );
 		add_action( 'wp_ajax_mpp_add_reply', array( $this, 'post_reply' ) );
-		
 	}
-	
+
 	/**
 	 * Post a gallery or media Main comment on single page
-	 * 
-	 * @return type
 	 */
 	public function post_comment() {
-		//this is BuddyPress dependent
+		// This is BuddyPress dependent.
 		if ( ! function_exists( 'buddypress' ) ) {
 			exit( 0 );
 		}
-		// Bail if not a POST action
+		// Bail if not a POST action.
 		if ( 'POST' !== strtoupper( $_SERVER['REQUEST_METHOD'] ) ) {
 			return;
 		}
-		
-		// Check the nonce
+
+		// Check the nonce.
 		check_admin_referer( 'post_update', '_wpnonce_post_update' );
 
 		if ( ! is_user_logged_in() ) {
 			exit( '-1' );
 		}
-		
+
 		$mpp_type = $_POST['mpp-type'];
-		$mpp_id = $_POST['mpp-id'];
-		
+		$mpp_id   = $_POST['mpp-id'];
+
 		if ( empty( $_POST['content'] ) ) {
 			exit( '-1<div id="message" class="error"><p>' . __( 'Please enter some content to post.', 'mediapress' ) . '</p></div>' );
 		}
-		
+
 		$activity_id = 0;
-		
+
 		if ( empty( $_POST['object'] ) && bp_is_active( 'activity' ) ) {
-			
-			//we are preventing this comment to be set as the user's lastes_update
+
+			// we are preventing this comment to be set as the user's lastes_update.
 			$user_id = bp_loggedin_user_id();
-			
+
 			$old_latest_update = bp_get_user_meta( $user_id, 'bp_latest_update', true );
-			
+
 			$activity_id = bp_activity_post_update( array( 'content' => $_POST['content'] ) );
-			//restore
+			// restore.
 			if ( ! empty( $old_latest_update ) ) {
 				bp_update_user_meta( $user_id, 'bp_latest_update', $old_latest_update );
 			}
-
-		} elseif ( $_POST['object'] == 'groups'  ) {
-			if ( ! empty( $_POST['item_id'] ) && bp_is_active( 'groups' ) )
-				$activity_id = groups_post_update( array( 'content' => $_POST['content'], 'group_id' => $_POST['item_id'] ) );
-
+		} elseif (  'groups' === $_POST['object'] ) {
+			if ( ! empty( $_POST['item_id'] ) && bp_is_active( 'groups' ) ) {
+				$activity_id = groups_post_update( array(
+					'content'  => $_POST['content'],
+					'group_id' => $_POST['item_id'],
+				) );
+			}
 		} else {
 			$activity_id = apply_filters( 'bp_activity_custom_update', $_POST['object'], $_POST['item_id'], $_POST['content'] );
 		}
@@ -92,48 +100,48 @@ class MPP_Ajax_Comment_Helper {
 			exit( '-1<div id="message" class="error"><p>' . __( 'There was a problem posting your update, please try again.', 'mediapress' ) . '</p></div>' );
 		}
 		$status = '';
-		//if we have got activity id, let us add a meta key
-		if ( $mpp_type == 'gallery' ) {
-			
+		// if we have got activity id, let us add a meta key.
+		if ( 'gallery' === $mpp_type ) {
+
 			mpp_activity_update_gallery_id( $activity_id, $mpp_id );
 			mpp_activity_update_activity_type( $activity_id, 'gallery_comment' );
 			mpp_activity_update_context( $activity_id, 'gallery' );
-			
+
 			$status = mpp_get_gallery_status( $mpp_id );
-			
-		} elseif ( $mpp_type == 'media' ) {
-			
+
+		} elseif ( 'media' === $mpp_type ) {
+
 			$media = mpp_get_media( $mpp_id );
-			
+
 			if ( ! $media ) {
-				die('-1');
+				die( '-1' );
 			}
 			mpp_activity_update_gallery_id( $activity_id, $media->gallery_id );
 			mpp_activity_update_media_id( $activity_id, $mpp_id );
 			mpp_activity_update_activity_type( $activity_id, 'media_comment' );
 			mpp_activity_update_context( $activity_id, 'media' );
-			//also we need to keep the parent gallery id for caching
-			
+
+			// also we need to keep the parent gallery id for caching.
 			$status = mpp_get_media_status( $media );
 		}
-		
-		 $activity = new BP_Activity_Activity( $activity_id );
+
+		$activity = new BP_Activity_Activity( $activity_id );
 		// $activity->component = buddypress()->mediapress->id;
-		 $activity->type = 'mpp_media_upload';
-		 $activity->save();
-		 
-		 //save activity privacy
-		 if ( $status ) {
-			 $status_object = mpp_get_status_object( $status );
-			 
+		$activity->type = 'mpp_media_upload';
+		$activity->save();
+
+		// save activity privacy.
+		if ( $status ) {
+			$status_object = mpp_get_status_object( $status );
+
 			if ( $status_object ) {
 				bp_activity_update_meta( $activity->id, 'activity-privacy', $status_object->activity_privacy );
 			}
-		 }
-		 //create a shadow comment
-		 mpp_activity_create_comment_for_activity( $activity_id );
-		
-		if ( bp_has_activities ( 'include=' . $activity_id ) ) {
+		}
+		// create a shadow comment.
+		mpp_activity_create_comment_for_activity( $activity_id );
+
+		if ( bp_has_activities( 'include=' . $activity_id ) ) {
 			while ( bp_activities() ) {
 				bp_the_activity();
 				mpp_locate_template( array( 'buddypress/activity/entry.php' ), true );
@@ -142,6 +150,7 @@ class MPP_Ajax_Comment_Helper {
 
 		exit;
 	}
+
 	/**
 	 * Posts new Activity comments received via a POST request.
 	 *
@@ -150,21 +159,21 @@ class MPP_Ajax_Comment_Helper {
 	 * @since BuddyPress (1.2)
 	 */
 	public function post_reply() {
-				
+
 		if ( ! function_exists( 'buddypress' ) ) {
 			exit( 0 );
 		}
-		
+
 		global $activities_template;
 
 		$bp = buddypress();
 
-		// Bail if not a POST action
+		// Bail if not a POST action.
 		if ( 'POST' !== strtoupper( $_SERVER['REQUEST_METHOD'] ) ) {
 			return;
 		}
 
-		// Check the nonce
+		// Check the nonce.
 		check_admin_referer( 'new_activity_comment', '_wpnonce_new_activity_comment' );
 
 		if ( ! is_user_logged_in() ) {
@@ -196,28 +205,28 @@ class MPP_Ajax_Comment_Helper {
 			exit( '-1<div id="message" class="error bp-ajax-message"><p>' . esc_html( $feedback ) . '</p></div>' );
 		}
 
-		// Load the new activity item into the $activities_template global
+		// Load the new activity item into the $activities_template global.
 		bp_has_activities( 'display_comments=stream&hide_spam=false&show_hidden=true&include=' . $comment_id );
 
 		// Swap the current comment with the activity item we just loaded
 		if ( isset( $activities_template->activities[0] ) ) {
-			$activities_template->activity = new stdClass();
+			$activities_template->activity                  = new stdClass();
 			$activities_template->activity->id              = $activities_template->activities[0]->item_id;
 			$activities_template->activity->current_comment = $activities_template->activities[0];
 
 			// Because the whole tree has not been loaded, we manually
 			// determine depth
-			$depth = 1;
+			$depth     = 1;
 			$parent_id = (int) $activities_template->activities[0]->secondary_item_id;
 			while ( $parent_id !== (int) $activities_template->activities[0]->item_id ) {
-				$depth++;
-				$p_obj = new BP_Activity_Activity( $parent_id );
+				$depth ++;
+				$p_obj     = new BP_Activity_Activity( $parent_id );
 				$parent_id = (int) $p_obj->secondary_item_id;
 			}
 			$activities_template->activity->current_comment->depth = $depth;
 		}
 
-		// get activity comment template part
+		// get activity comment template part.
 		mpp_get_template_part( 'buddypress/activity/comment' );
 
 		unset( $activities_template );
@@ -225,5 +234,5 @@ class MPP_Ajax_Comment_Helper {
 	}
 }
 
-//initialize
+// initialize.
 MPP_Ajax_Comment_Helper::get_instance();
--- core/ajax/class-mpp-ajax-lightbox-helper.php
+++ core/ajax/class-mpp-ajax-lightbox-helper.php
@@ -1,6 +1,6 @@
 <?php
 
-// Exit if the file is accessed directly over web
+// Exit if the file is accessed directly over web.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
@@ -8,37 +8,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * MediaPress Ajax helper
  * Not implementing it as singleton, if you need to add custom handler, attach your own with higher priority
- * 
  */
 class MPP_Ajax_Lightbox_Helper {
 
+	/**
+	 * Template directory(cached)
+	 *
+	 * @var string
+	 */
 	private $template_dir;
-	
-	public function __construct () {
-		
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+
 		$this->template_dir = mediapress()->get_path() . 'admin/templates/';
-		
+
 		$this->setup_hooks();
 	}
 
-	
-	private function setup_hooks () {
-		//activity media
+	/**
+	 * Setup.
+	 */
+	private function setup_hooks() {
+		// activity media.
 		add_action( 'wp_ajax_mpp_fetch_activity_media', array( $this, 'fetch_activity_media' ) );
 		add_action( 'wp_ajax_nopriv_mpp_fetch_activity_media', array( $this, 'fetch_activity_media' ) );
-		//for lightbox when clicked on gallery		
+		// for lightbox when clicked on gallery.
 		add_action( 'wp_ajax_mpp_fetch_gallery_media', array( $this, 'fetch_gallery_media' ) );
 		add_action( 'wp_ajax_nopriv_mpp_fetch_gallery_media', array( $this, 'fetch_gallery_media' ) );
 
 		add_action( 'wp_ajax_mpp_lightbox_fetch_media', array( $this, 'fetch_media' ) );
 		add_action( 'wp_ajax_nopriv_mpp_lightbox_fetch_media', array( $this, 'fetch_media' ) );
-		
 	}
 
+	/**
+	 * Get media fro activity
+	 */
+	public function fetch_activity_media() {
 
-	public function fetch_activity_media () {
-		
-		//do we need nonce validation for this request too? no
+		// do we need nonce validation for this request too?
 		$items = array();
 		$activity_id = $_POST['activity_id'];
 
@@ -49,31 +59,30 @@ class MPP_Ajax_Lightbox_Helper {
 		$media_ids = mpp_activity_get_attached_media_ids( $activity_id );
 
 		if ( empty( $media_ids ) ) {
-			
+
 			array_push( $items, __( 'Sorry, Nothing found!', 'mediapress' ) );
-			
+
 			wp_send_json( array( 'items' => $items ) );
 			exit( 0 );
 		}
 
 		$gallery_id = mpp_activity_get_gallery_id( $activity_id );
-		$gallery	= mpp_get_gallery( $gallery_id );
-		
-		if ( $gallery->component == 'groups' && function_exists( 'bp_is_active' ) && bp_is_active( 'groups' ) ) {
+		$gallery    = mpp_get_gallery( $gallery_id );
+
+		if ( 'groups' === $gallery->component && function_exists( 'bp_is_active' ) && bp_is_active( 'groups' ) ) {
 			//if( empty( buddypress()->groups))
 		}
 
-		$media_query = new MPP_Media_Query( array( 'in' => $media_ids,'per_page'=> 0 ) );
-		
-		if ( $media_query->have_media() ):
+		$media_query = new MPP_Media_Query( array( 'in' => $media_ids, 'per_page' => 0 ) );
+
+		if ( $media_query->have_media() ) :
 			?>
 
+			<?php while ( $media_query->have_media() ) : $media_query->the_media(); ?>
 
-			<?php while ( $media_query->have_media() ): $media_query->the_media(); ?>
+			<?php $items[] = array( 'src' => $this->get_media_lightbox_entry() ); ?>
 
-				<?php $items[] = array( 'src' => $this->get_media_lightbox_entry() ); ?>
-
-			<?php endwhile; ?>
+		<?php endwhile; ?>
 
 		<?php endif; ?>
 		<?php mpp_reset_media_data(); ?>
@@ -83,31 +92,37 @@ class MPP_Ajax_Lightbox_Helper {
 		exit( 0 );
 	}
 
-	public function fetch_gallery_media () {
-		
-		//do we need nonce validation for this request too? no
+	/**
+	 * Fetch for gallery.
+	 */
+	public function fetch_gallery_media() {
+
+		// do we need nonce validation for this request too? no.
 		$items = array();
-		
+
 		$gallery_id = absint( $_POST['gallery_id'] );
-		$gallery = mpp_get_gallery( $gallery_id );
-		
+		$gallery    = mpp_get_gallery( $gallery_id );
+
 		if ( ! $gallery_id || empty( $gallery ) ) {
 			exit( 0 );
 		}
-		
+
 		$statuses = mpp_get_accessible_statuses( $gallery->component, $gallery->component_id, get_current_user_id() );
-		
-		$media_query = new MPP_Media_Query( array( 'gallery_id' => $gallery_id, 'per_page' => 0, 'status' => $statuses ) );
-		
-		if ( $media_query->have_media() ):
+
+		$media_query = new MPP_Media_Query( array(
+			'gallery_id' => $gallery_id,
+			'per_page'   => 0,
+			'status'     => $statuses,
+		) );
+
+		if ( $media_query->have_media() ) :
 			?>
 
+			<?php while ( $media_query->have_media() ) : $media_query->the_media(); ?>
 
-			<?php while ( $media_query->have_media() ): $media_query->the_media(); ?>
+			<?php $items[] = array( 'src' => $this->get_media_lightbox_entry(), 'id' => mpp_get_media_id() ); ?>
 
-				<?php $items[] = array( 'src' => $this->get_media_lightbox_entry(), 'id'=> mpp_get_media_id() ); ?>
-
-			<?php endwhile; ?>
+		<?php endwhile; ?>
 
 		<?php endif; ?>
 		<?php mpp_reset_media_data(); ?>
@@ -117,8 +132,11 @@ class MPP_Ajax_Lightbox_Helper {
 		exit( 0 );
 	}
 
+	/**
+	 * Fetch individual media or media list.
+	 */
 	public function fetch_media() {
-		//do we need nonce validation for this request too? no
+		// do we need nonce validation for this request too? no.
 		$items = array();
 
 		$media_ids = $_POST['media_ids'];
@@ -129,54 +147,57 @@ class MPP_Ajax_Lightbox_Helper {
 		}
 
 
-		$media_query = new MPP_Media_Query( array( 'in' => $media_ids, 'per_page' => 0, 'orderby' => 'none') );
-		$user_id = get_current_user_id();
+		$media_query = new MPP_Media_Query( array( 'in' => $media_ids, 'per_page' => 0, 'orderby' => 'none' ) );
+		$user_id     = get_current_user_id();
 
-		if ( $media_query->have_media() ):
+		if ( $media_query->have_media() ) :
 			?>
 
 
-			<?php while ( $media_query->have_media() ): $media_query->the_media(); ?>
+			<?php while ( $media_query->have_media() ) : $media_query->the_media(); ?>
 
-				<?php
+			<?php
+			if ( ! mpp_user_can_view_media( mpp_get_media_id(), $user_id ) ) {
+				continue;
+			}
 
+			?>
+			<?php $items[ mpp_get_media_id() ] = array( 'src' => $this->get_media_lightbox_entry() ); ?>
 
-					if ( ! mpp_user_can_view_media( mpp_get_media_id(), $user_id ) ) {
-							continue;
-					}
-
-				?>
-				<?php $items[ mpp_get_media_id() ] = array( 'src' => $this->get_media_lightbox_entry() ); ?>
-
-			<?php endwhile; ?>
+		<?php endwhile; ?>
 
 		<?php endif; ?>
 
 		<?php mpp_reset_media_data(); ?>
 		<?php
-		//reorder items according to our ids order, WP resets to desc order
-
+		// reorder items according to our ids order, WP resets to desc order.
 		$new_items = array();
-		//it may not be the best way but it seems to be the only way to make it work where we should not order media at all
-		foreach( $media_ids as $media_id ) {
-			if ( isset( $items[$media_id ] ) ) {
-				$new_items[] = $items[$media_id];
+		// it may not be the best way but it seems to be the only way to make it work where we should not order media at all.
+		foreach ( $media_ids as $media_id ) {
+			if ( isset( $items[ $media_id ] ) ) {
+				$new_items[] = $items[ $media_id ];
 			}
 		}
 
-		wp_send_json( array( 'items' => $new_items) );
+		wp_send_json( array( 'items' => $new_items ) );
 		exit( 0 );
 	}
-	private function get_media_lightbox_entry () {
+
+	/**
+	 * Entry for individual media.
+	 *
+	 * @return string
+	 */
+	private function get_media_lightbox_entry() {
 
 		ob_start();
 
 		mpp_get_template_part( 'gallery/media/views/lightbox-comment' );
-		
+
 		return ob_get_clean();
 	}
-	
+
 }
 
-//initialize
+// initialize.
 new MPP_Ajax_Lightbox_Helper();
