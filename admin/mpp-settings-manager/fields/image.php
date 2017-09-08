a433601031ea8d82c9baf3f04bb397753745d4c7
Brajesh Singh <brajesh@cosmiccoders.com>
2017-09-08 15:50:39 +0530

    Cleanup image field and fix the notice when  was not initialized

--- admin/mpp-settings-manager/fields/image.php
+++ admin/mpp-settings-manager/fields/image.php
@@ -1,56 +1,77 @@
 <?php
-// Exit if the file is accessed directly over web
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; 
-}
 /**
- * Used for Image field type
+ * Image setting field.
+ *
+ * @package mediapress/settings/fields
+ */
+
+// Exit if the file is accessed directly over web.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 0 );
+}
+
+/**
+ * Image field type.
  */
 class MPP_Admin_Settings_Field_Image extends MPP_Admin_Settings_Field {
-    
-    
-    public function __construct( $field ) {
-		
-        parent::__construct( $field );
-		
-    }
-    
-    
-    public function render( $args ) {
-        wp_enqueue_media();
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $field field details array.
+	 */
+	public function __construct( $field ) {
+		parent::__construct( $field );
+	}
+
+	/**
+	 * Render field ui.
+	 *
+	 * @param array $args args.
+	 */
+	public function render( $args ) {
+	    // Load js.
+		wp_enqueue_media();
 		wp_enqueue_script( 'mpp_settings_uploader' );
-        $value = esc_attr( $args['value'] );//atgtachment url
-        $size = $this->get_size();
-        
-        //we need to show this imaage
-        if( $value ) {
-           $image = "<img src='{$value}' />";
-        }else {
-			
-            $image = "<img src='' />";
-        }
-        
-        $id = $args['option_key'];
-		
+
+		// attachment url.
+		$value = esc_attr( $args['value'] );
+		$size  = $this->get_size();
+		// css class.
+		$class = '';
+		// we need to show this image.
+		if ( $value ) {
+			$image = "<img src='{$value}' />";
+		} else {
+
+			$image = "<img src='' />";
+		}
+
+		$id = $args['option_key'];
+
 		?>
 
-		<div class='settings-image-placeholder'>
-			<?php 
-				if( $value )
-					$class = 'settings-image-action-visible';
-               
-                echo $image;
-            ?>
-			<br />
-			<a href="#" class="delete-settings-image <?php echo $class;?>"><?php _e( 'Remove' );?></a> 
-		</div>
+        <div class='settings-image-placeholder'>
+			<?php
+			if ( $value ) {
+				$class = 'settings-image-action-visible';
+			}
+
+			echo $image;
+			?>
+            <br/>
+            <a href="#" class="delete-settings-image <?php echo $class; ?>"><?php _e( 'Remove' ); ?></a>
+        </div>
 
 		<?php
-			echo  "<input type='hidden' class='hidden-image-url' id='{$id}' name='{$id}' value='{$value}'/>";
-			echo  '<input type="button" class="button settings-upload-image-button" id="'. $id .'_button" value="Browse" data-id="'.$id.'" data-btn-title="Select" data-uploader-title="Select" />';
-       
-			echo '<span class="description">'. $this->get_desc() . '</span>';
+		$btn_label      = _x( 'Select', 'mediapress settings image field select button label', 'mediapress' );
+		$browse_label   = _x( 'Browse', 'mediapress settings image field browse', 'mediapress' );
+		$uploader_title = _x( 'Select', 'mediapress settings image field uploader window title', 'mediapress' );
 
+		echo "<input type='hidden' class='hidden-image-url' id='{$id}' name='{$id}' value='{$value}'/>";
+		echo "<input type='button' class='button settings-upload-image-button' id='{$id}_button' value='{$browse_label}' data-id='{$id}' data-btn-title='{$btn_label}' data-uploader-title='{$uploader_title}' />";
+
+		echo '<span class="description">' . $this->get_desc() . '</span>';
         
-    }
+	}
 }
