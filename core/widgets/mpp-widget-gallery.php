e1fae6b54b5e20e1d11c993915d71b9a797b07d3
Brajesh Singh <brajesh@cosmiccoders.com>
2018-03-24 08:17:55 +0530

    Add the User context to gallery widget

--- core/widgets/mpp-widget-gallery.php
+++ core/widgets/mpp-widget-gallery.php
@@ -97,19 +97,30 @@ class MPP_Gallery_List_Widget extends WP_Widget {
 			// 'meta_query'=>false,
 			// which fields to return ids, id=>parent, all fields(default).
 			'fields'        => false,
+			'for'           => '',
 		);
 
 		$instance = (array) $instance;
-
-		echo $args['before_widget'];
-
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'] . $instance['title'] . $args['after_title'];
-		}
-
+		$title = $instance['title'];
 		unset( $instance['title'] );
 
 		$query_args = array_merge( $defaults, $instance );
+
+		$for = $query_args['for'];
+		unset( $query_args['for'] );
+
+		if ( $for ) {
+			$query_args['user_id'] = mpp_get_dynamic_user_id_for_context( $for );
+			if ( empty( $query_args['user_id'] ) ) {
+				return;
+			}
+		}
+
+		echo $args['before_widget'];
+
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
 
 		$query = new MPP_Gallery_Query( $query_args );
 
@@ -153,6 +164,7 @@ class MPP_Gallery_List_Widget extends WP_Widget {
 		$instance['orderby'] = $new_instance['orderby'];
 
 		$instance['order'] = $new_instance['order'];
+		$instance['for'] = $new_instance['for'];
 
 		return $instance;
 	}
@@ -184,7 +196,7 @@ class MPP_Gallery_List_Widget extends WP_Widget {
 			// the associated component id, could be group id, user id, event id.
 			'component_id'  => false,
 			// how many items per page.
-			'per_page'      => false,
+			'per_page'      => 5,
 			// how many galleries to offset/displace.
 			'offset'        => false,
 			// which page when paged.
@@ -227,6 +239,7 @@ class MPP_Gallery_List_Widget extends WP_Widget {
 			'fields'        => false,
 			'column'        => 4,
 			'title'         => __( 'Recent Galleries', 'mediapress' ),
+			'for'           => '',
 		);
 
 		$instance = wp_parse_args( (array) $instance, $defaults );
@@ -282,6 +295,22 @@ class MPP_Gallery_List_Widget extends WP_Widget {
 						'selected' => $instance['status'],
 					) );
 					?>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="<?php echo $this->get_field_id( 'for' ); ?>"><?php _e( 'Galleries of:', 'mediapress' ); ?></label>
+                </td>
+                <td>
+                    <select id="<?php echo $this->get_field_id( 'for' ); ?>"
+                            name="<?php echo $this->get_field_name( 'for' ); ?>">
+                        <option value="" <?php selected( '', $instance['for'] ); ?>><?php _e( 'Everyone', 'mediapress' ); ?></option>
+                        <option value="logged" <?php selected( 'logged', $instance['for'] ); ?>><?php _e( 'Logged In User', 'mediapress' ); ?></option>
+		                <?php if ( mediapress()->is_bp_active() ) : ?>
+                            <option value="displayed" <?php selected( 'displayed', $instance['for'] ); ?>><?php _e( 'Displayed User', 'mediapress' ); ?></option>
+		                <?php endif;?>
+                    </select>
+
                 </td>
             </tr>
             <tr>
