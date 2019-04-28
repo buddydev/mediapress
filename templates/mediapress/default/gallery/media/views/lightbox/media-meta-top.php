e8afa9edfd0b96a4939f36ddd3ad02ebdb5b1c8b
Brajesh Singh <brajesh@cosmiccoders.com>
2019-04-28 23:36:51 +0530

    Do not show top metabox if there is nothing to show

--- templates/mediapress/default/gallery/media/views/lightbox/media-meta-top.php
+++ templates/mediapress/default/gallery/media/views/lightbox/media-meta-top.php
@@ -4,7 +4,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 $media = mpp_get_current_media();
+ob_start();
+do_action( 'mpp_lightbox_media_meta_top', $media );
+
+$meta_top = ob_get_clean();
 ?>
-<div class="mpp-item-meta mpp-media-meta mpp-lightbox-media-meta mpp-lightbox-media-meta-top">
-	<?php do_action( 'mpp_lightbox_media_meta_top', $media ); ?>
-</div>
+<?php if ( $meta_top ): ?>
+    <div class="mpp-item-meta mpp-media-meta mpp-lightbox-media-meta mpp-lightbox-media-meta-top">
+		<?php echo $meta_top; ?>
+    </div>
+<?php endif; ?>
