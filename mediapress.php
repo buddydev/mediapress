7a22a71ca2d2a91288f91f41bd3ec71c19179770
raviousprime <raviousprime@gmail.com>
2023-04-03 15:49:29 +0530

    version update

--- mediapress.php
+++ mediapress.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: MediaPress
- * Version: 1.5.4
+ * Version: 1.5.5
  * Author: BuddyDev
  * Plugin URI: https://buddydev.com/mediapress/
  * Author URI: https://buddydev.com
