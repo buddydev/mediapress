1a9de7e41bf41fe601cbc741dcaf60659beb7d3f
PressProgrammer <dev@pressprogrammer.com>
2025-08-26 02:28:45 +0530

    Update version in plugin header

--- mediapress.php
+++ mediapress.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: MediaPress
- * Version: 1.5.9.1
+ * Version: 1.6.0
  * Author: BuddyDev
  * Plugin URI: https://buddydev.com/mediapress/
  * Author URI: https://buddydev.com
